e00d325505e26cd8f53fb429c7dcbc77a451690b
root <root@DESKTOP-1IKG3KL.localdomain>
2021-06-04 16:08:44 +0300

    Header accessibility

--- js/art-direction-select.js
+++ js/art-direction-select.js
@@ -1,11 +1,18 @@
 window.addEventListener('DOMContentLoaded', function() {
+
     document.querySelectorAll('.art-direction-select').forEach(function(el) {
         const choices = new Choices(el, {
             searchEnabled: false,
-        })
-    })
+            shouldSort: false,
+        });
+    });
 
     document.querySelectorAll('.choices__list--dropdown').forEach(function(el) {
         el.dataset.simplebar = '';
-    }) 
+    });
+    
+    document.querySelectorAll('.art-direction-select').forEach(function(el) {
+        let ariaLabel = el.getAttribute('aria-label');
+        el.closest('.choices').setAttribute('aria-label', ariaLabel);
+    });
 })
