776ff71910a5448b9654186a91a4dba256a08fc7
root <root@DESKTOP-1IKG3KL.localdomain>
2021-10-20 09:38:45 +0300

    Реализация модальных окон
    - модальные окна в gallery-slider;
    - модальное окно при отправке формы в contacts.

--- js/gallery-slider.js
+++ js/gallery-slider.js
@@ -1,38 +1,67 @@
-window.addEventListener('DOMContentLoaded', function() {
-
-  var gallerySlider = new Swiper('.gallery-slider__container', {
-        
+document.addEventListener('DOMContentLoaded', function() {
+    var gallerySlider = new Swiper('.gallery-slider__container', {
         direction: 'horizontal',
         loop: true,
         slidesPerColumnFill: 'row',
         updateOnWindowResize: true,
         
         pagination: {
-          el: '.gallery-slider__pagination',
-          type: 'fraction',
+			el: '.gallery-slider__pagination',
+			type: 'fraction',
         },
       
         navigation: {
-          nextEl: '.gallery-slider__nav-btn-next',
-          prevEl: '.gallery-slider__nav-btn-prev',
+			nextEl: '.gallery-slider__nav-btn-next',
+			prevEl: '.gallery-slider__nav-btn-prev',
         },
         
         breakpoints: {
-          768: {
-            loop: false,
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-            slidesPerColumn: 2,
-            spaceBetween: 34,
-          },
-          1440: {
-            loop: false,
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-            slidesPerColumn: 2,
-            spaceBetween: 50,
-            speed: 600,
-          },
+			768: {
+			loop: false,
+			slidesPerView: 2,
+			slidesPerGroup: 2,
+			slidesPerColumn: 2,
+			spaceBetween: 34,
+			},
+			1440: {
+			loop: false,
+			slidesPerView: 3,
+			slidesPerGroup: 3,
+			slidesPerColumn: 2,
+			spaceBetween: 50,
+			speed: 600,
+			},
         }
     });
-})
+
+	let body = document.querySelector('body');
+	let btn = document.querySelectorAll('.gallery-modal-btn');
+	let modal = document.querySelector('.gallery-modals');
+	let close = document.querySelector('.gallery-modals__close-btn');
+	
+	btn.forEach((elem) => {
+		elem.addEventListener('click', (e) => {
+			let path = e.currentTarget.getAttribute('data-path');
+			let target = document.querySelector(`[data-target=${path}]`);
+
+			modal.classList.add('modal--visible');
+			target.classList.add('gallery-modals__item--visible');
+			body.classList.add('body-hidden');
+
+			console.log(path);
+			console.log(e);
+		});
+
+		close.addEventListener('click', () => {
+			let target = document.querySelectorAll('.modal__item');
+
+			target.forEach((elem) => {
+				if (elem.classList.contains('gallery-modals__item--visible')) {
+					elem.classList.remove('gallery-modals__item--visible');
+				}
+			});
+			modal.classList.remove('modal--visible');
+			body.classList.remove('body-hidden');
+		});
+	});
+});
